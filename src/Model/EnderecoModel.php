f6eb048bdde898c2de94dfa45384a9f2aad9e506
Gustavo Luiz Farignoli <gustavolf.educ@gmail.com>
2024-10-15 12:00:07 -0300

    Post de Endereco, Agora com Complimento e Pais, Porém o front não é capaz de detectar quando o back retorna 500

--- src/Model/EnderecoModel.php
+++ src/Model/EnderecoModel.php
@@ -5,7 +5,7 @@ use Config\Conexao;
 class EnderecoModel extends Model
 {
     protected string $allDataSql = "SELECT * FROM enderecos";
-    protected string $createSql = "INSERT INTO enderecos (enderecos.cep, enderecos.rua, enderecos.numero, enderecos.cidade, enderecos.estado) VALUES (?, ?, ?, ?, ?)";
+    protected string $createSql = "INSERT INTO enderecos (enderecos.cep, enderecos.rua, enderecos.numero, enderecos.cidade, enderecos.estado, enderecos.pais, endereocs.complemento) VALUES (?, ?, ?, ?, ?, ?, ?)";
 
     public function __construct(int $id = -1) {
         $this->singleDataSql = "SELECT * FROM enderecos WHERE enderecos.id_endereco = $id";
@@ -19,7 +19,8 @@ class EnderecoModel extends Model
             $preparedSql->bindValue(param: 3, value: $postData["numero"], type: \PDO::PARAM_INT);
             $preparedSql->bindValue(param: 4, value: $postData["cidade"], type: \PDO::PARAM_STR);
             $preparedSql->bindValue(param: 5, value: $postData["estado"], type: \PDO::PARAM_STR);
-            //$preparedSql->bindValue(param: 6, value: $postData["pais"], type: \PDO::PARAM_STR);
+            $preparedSql->bindValue(param: 6, value: $postData["pais"], type: \PDO::PARAM_STR);
+            $preparedSql->bindValue(param: 7, value: $postData["complemento"], type: \PDO::PARAM_STR);
             
             $success = $preparedSql->execute();
             header('Content-Type: application/json');
