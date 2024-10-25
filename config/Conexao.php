76cc7723512c456f3c686038cf68e8f66562e5fa
Gbex <gabriel.mocellin204@gmail.com>
2024-10-25 14:43:21 -0300

    dotEnv funcionando para conexao

--- config/Conexao.php
+++ config/Conexao.php
@@ -1,44 +1,69 @@
 <?php
 namespace Config;
 use Dotenv\Dotenv;
+use Exception;
 
 class Conexao
 { 
-    private static $instance;
-    
-    private static \PDO $conexao;
+    private static ?Conexao $instance = null;
+    private static ?\PDO $conexao = null;
 
-    protected function __construct() { }
 
-    public static function getInstance(): Conexao
+    protected function __construct() {
+    }
+
+    public static function getInstance(): ?Conexao
     {
-        if (isset(self::$instance)) return self::$instance;
-        return self::createInstance();
+        if (self::$instance === null) {
+            try {
+                self::$instance = self::createInstance();
+            } catch (Exception $e) {
+                error_log(message: "Erro ao criar instância de conexão: " . $e->getMessage());
+                return null;
+            }
+        }
+        return self::$instance;
     }
 
     protected static function createInstance(): Conexao
     {
-        $cls = static::class;
-        self::$instance = new $cls();
-        self::$instance::createConexao();
+        $connectionSetted = self::setNewConnection();
+        if (!$connectionSetted) {
+            throw new Exception("Não foi possível criar a conexão com o banco!");
+        }
+        self::$instance = new self();
         return self::$instance;
     }
 
-    public static function getConexao(): \PDO {
+    public static function getConexao(): \PDO
+    {
+        if (!isset(self::$conexao)) {
+            throw new Exception(message: "Conexão ainda não foi estabelecida.");
+        }
         return self::$conexao;
     }
 
-    public static function createConexao(): bool {        
-        $user = "root";
-        $pass = "EgSoftware#24";
-        $dbname = "billigkjop";
-        $dbip = "localhost";
+    public static function setNewConnection(): bool {
+        self::loadDotEnv();
+        $user = $_ENV["USER"];
+        $pass = $_ENV["PASS"];
+        $dbip = $_ENV["DBIP"];
+        $dbname = $_ENV["DBNAME"];
         try {
             self::$conexao = new \PDO(dsn: "mysql:host=$dbip;dbname=$dbname", username: $user, password: $pass);
             return true;
         } catch (\PDOException $e) {
-            error_log(message: "Erro de conexão ao banco de dados: " . $e->getMessage());
+            error_log(message: "Erro ao criar conexão com banco de dados: " . $e->getMessage());
+            return false;
+        } catch (Exception $exception) {
+            error_log(message: "Erro desconhecido: " . $exception->getMessage());
             return false;
         }
+    }
+
+    public static function loadDotEnv(): void {
+        $envPath = dirname(path: __DIR__, levels: 1);
+        $dotenv = Dotenv::createImmutable($envPath);
+        $dotenv->safeLoad();
     }
 }
